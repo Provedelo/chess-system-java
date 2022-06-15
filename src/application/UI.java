66d55868d4b7e5209ff79cba9b0077fc29d12816
Angelo Provedelo <angelo_provedelo@hotmail.com>
2022-06-14 23:59:05 -0300

    correct error at nullExceptionPoint

--- src/application/UI.java
+++ src/application/UI.java
@@ -105,8 +105,10 @@ public class UI {
 	}
 	
 	private static void printCapturedPiece(List<ChessPiece> captured) {
+		
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList()); //filter by collor
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+		
 		System.out.println("Captured pieces: ");
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
