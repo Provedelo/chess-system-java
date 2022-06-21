3ded960dc61d5b515fa9387dcc991b1c9eb4df0a
Angelo Provedelo <angelo_provedelo@hotmail.com>
2022-06-21 13:59:35 -0300

    Pawn addicted and moves rules

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -193,6 +194,7 @@ public class ChessMatch {
 		placeNewPiece('h', 7, new Rook(board, Color.WHITE));
         placeNewPiece('d', 1, new Rook(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('a', 1, new Pawn(board, Color.WHITE));
         
         placeNewPiece('d', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 8, new King(board, Color.BLACK));
